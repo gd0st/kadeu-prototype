c87970cc0148adc1d512e131dbf8c68b36b7466c
Yannick <yannick.dorn@gmail.com>
2023-03-04 23:51:15 -0600

    Got the project back to the basics for now.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Style},
-    text::{Span, Spans},
+    text::{Span, Spans, Text},
     widgets::{Block, Borders, List, ListItem, Paragraph, Widget},
     Terminal,
 };
@@ -70,252 +70,68 @@ impl From<config::Config> for DeckFsConfig {
     }
 }
 
-fn main() -> io::Result<()> {
-    // What is the beginning of a user interface?
-    // As in what is the bare minimum interface for a program to be built upon?
-    // I think I need t accomplish making the program usable through command line arguments.
-    // Afterwards the game needs to have a comprehensive interactive interface.
-    // The interface will start with just stdio and the advance to the TUI
-    // After the tui has been accomplished and features seem good enough,
-    // The web browser and other parts can be accomplished
+// not really sure what to call this
+struct Indexer {
+    deck_list: Vec<String>,
+    db: DeckFs,
+}
 
+fn new_db() -> io::Result<DeckFs> {
     let config: config::Config = config::get_config().unwrap();
 
     let fs_config: DeckFsConfig = config.into();
-    let db: DeckFs = DeckFs::new(fs_config)?;
+    DeckFs::new(fs_config)
+}
 
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = tui::Terminal::new(backend)?;
-    let mut deck_titles = db.titles();
-    let mut input = String::new();
-    let mut selected_deck: Option<objects::Deck> = None;
+fn main() -> io::Result<()> {
+    let db = new_db()?;
 
-    loop {
-        terminal.draw(|frame| {
-            match input.as_str() {
-                ":q" => {
-                    return;
-                }
-                ":reload" => {
-                    println!("Reloading!");
-                    deck_titles = db.titles();
-                }
-                _ => {}
-            }
+    let decks = db.get_decks();
 
-            if deck_titles.iter().any(|x| x == &input) {
-                println!("Found a Deck!");
-                return;
-            };
-
-            let mut items: Vec<ListItem> = Vec::new();
-
-            for title in db.titles() {
-                if title == input {
-                    selected_deck = db.find_from_string(input.clone());
-                }
-                items.push(ListItem::new(title));
-            }
-
-            // Simple black background.
-            //let background = Layout::default()
-            //.constraints([Constraint::Percentage(100)].as_ref())
-            //.split(frame.size());
-            //let backing = Block::default().style(Style::default().bg(Color::Black));
-            //
-            //frame.render_widget(backing, background[0]);
-
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .margin(1)
-                .constraints(
-                    [
-                        Constraint::Percentage(30),
-                        Constraint::Percentage(30),
-                        Constraint::Percentage(30),
-                    ]
-                    .as_ref(),
-                )
-                .split(frame.size());
-
-            // Horizontal split into thirds.
-            // Then Vertical split into quarters.
-            let top_layout = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(1)
-                .constraints([
-                    Constraint::Percentage(25),
-                    Constraint::Percentage(25),
-                    Constraint::Percentage(25),
-                    Constraint::Percentage(25),
-                ])
-                .split(chunks[1]);
-
-            let simple_block = Block::default()
-                .borders(Borders::NONE)
-                .style(Style::default().bg(Color::Black));
-            let text = vec![Spans::from(Span::raw(input.as_str()))];
-            let deck_list = List::new(items).block(simple_block.clone());
-            let paragraph = Paragraph::new(text).block(simple_block.clone());
-            //let decks_list = List::new(items).block(paragraph_block);
-            frame.render_widget(deck_list, top_layout[1]);
-            frame.render_widget(paragraph, top_layout[2]);
-        });
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Enter => {
-                    if let Some(deck) = selected_deck {
-                        println!("Deck has been selected!");
-                        disable_raw_mode()?;
-                        return Ok(());
-                    } else {
-                        println!("Failed to select a deck!");
-                        disable_raw_mode()?;
-                        return Ok(());
-                    }
-                }
-                KeyCode::Char(c) => input.push(c),
-                KeyCode::Backspace => {
-                    input.pop();
-                }
-                KeyCode::Esc => {
-                    disable_raw_mode()?;
-                    return Ok(());
-                }
-                _ => {}
-            }
-        } else {
-            disable_raw_mode()?;
-            return Ok(());
-        }
+    println!("Available Decks:");
+    let mut count: usize = 0;
+    for deck in &decks {
+        count += 1;
+        println!("{}: {}", count, deck.title);
     }
-    return Ok(());
-    let mut input = read_and_strip();
+    let mut deck_index: usize = 0;
 
-    terminal.draw(|frame| {
-        let chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .constraints([Constraint::Percentage(100)].as_ref())
-            .split(frame.size());
+    while deck_index < 1 || deck_index > count + 1 {
+        println!("Select [1 - {}]", count);
+        deck_index = read_and_strip()?.parse().unwrap_or(0)
+    }
 
-        let paragraph_block = Block::default()
-            .title("Welcome to the World!")
-            .borders(Borders::ALL)
-            .style(Style::default().bg(Color::Black));
-        let paragraph = Paragraph::new("Hello World!").block(paragraph_block);
-        frame.render_widget(paragraph, chunks[0])
-    });
+    let game_deck = decks[deck_index - 1];
+    let mut cards = game_deck.cards.clone();
+    cards.shuffle(&mut thread_rng());
+    for card in &cards {
+        println!("{}:", card.challenge);
+        let input = read_and_strip()?;
 
-    disable_raw_mode()?;
-
-    //Need randomizer
-    // Game mode would have to be selected somewhere here.
-    if let Some(game_deck) = db.find_from_string(input) {
-        // Schedule would need to added somewhere in here for randommization.
-
-        let mut cards = game_deck.cards;
-        println!("{:?}", &cards[0]);
-        cards.shuffle(&mut thread_rng());
-        println!("{:?}", &cards[0]);
-        for card in cards {
-            println!("{}:", card.challenge);
-            input = read_and_strip();
-            if card.targets.iter().any(|x| x == &input) {
-                println!("Yes!")
-            } else {
-                println!("No :(");
-                println!("Acceptable Answers");
-                card.targets.iter().for_each(|x| println!("{}", x));
-            }
+        if card.targets.iter().any(|target| target == &input) {
+            println!("Correct")
+        } else {
+            println!("Incorrect");
+            println!("Answers:");
+            card.targets
+                .iter()
+                .for_each(|target| println!("{}", target));
         }
     }
 
     Ok(())
-
-    //let valid_deck_paths = DeckPaths::new(&config).unwrap();
-    //let decks: Vec<Deck> = valid_deck_paths
-    //.paths
-    //.iter()
-    //.map(|x| Deck::from(x))
-    //.collect();
-    //let mut deck: Option<&Deck> = None;
-    //
-    //while deck.is_none() {
-    //for path in decks.iter() {
-    //println!("{}", path.title());
-    //}
-    //println!("Please Choose A Deck:");
-    //let input = read_and_strip();
-    //let mut temp: Vec<&Deck> = decks.iter().filter(|x| *x.title() == input).collect();
-    //deck = temp.pop();
-    //}
-    //
-    //
-    //if let Some(deck) = deck {
-    //println!("Found a deck! {}", deck.title())
-    //}
-
-    //let file_meta = fs::metadata(&user_path).expect("Could not get metadata for user path");
-    //
-    //if file_meta.is_dir() {
-    //println!("Found a directory!")
-    //} else {
-    //println!("Not a directory!")
-    //}
-    //
-    //let directory =
-    //fs::read_dir(&user_path).expect("Could not list dir entry for Kadeu directory.");
-    //
-    //let mut decks: Vec<Deck> = Vec::new();
-    //for entry in directory {
-    //let unwrapped = entry.unwrap().path();
-    //if let Ok(mut file) = File::open(&unwrapped) {
-    //let mut buff = String::new();
-    //file.read_to_string(&mut buff).unwrap();
-    //let deck: serde_json::Result<Deck> = serde_json::from_str(&buff);
-    //match deck {
-    //Ok(deck) => {
-    //decks.push(deck);
-    //}
-    //Err(_) => eprintln!("{:?} Invalid deck.", &unwrapped),
-    //}
-    //}
-    //// Check the files if they are valid during the output.
-    //}
-    //
-    ////better
-    //decks.iter().for_each(|x| println!("{}", x.title()));
-    //println!("Please Select:");
-    //let input = read_strip()
-    //
-    //let mut possible_decks: Vec<&Deck> = decks.iter().filter(|x| x.title().eq(&input)).collect();
-    //
-    //if let Some(deck) = possible_decks.pop() {
-    //// let game = init_game(deck, 0);
-    //println!("Game is meant to start here.");
-    //use schedule::Schedule;
-    //let sequence = schedule::Random::schedule(deck.cards());
-    //for card in sequence {
-    //println!("Question: ");
-    //println!("{}", answer)
-    //}
-    //}
 }
 
-fn read_and_strip() -> String {
-    let mut buff_select = String::new();
-    io::stdin().read_line(&mut buff_select).unwrap();
+fn read_and_strip() -> io::Result<String> {
+    let mut buff = String::new();
+    io::stdin().read_line(&mut buff)?;
 
-    if let Some('\n') = buff_select.chars().next_back() {
-        buff_select.pop();
+    if let Some('\n') = buff.chars().next_back() {
+        buff.pop();
     }
-    if let Some('\r') = buff_select.chars().next_back() {
-        buff_select.pop();
+    if let Some('\r') = buff.chars().next_back() {
+        buff.pop();
     }
 
-    buff_select
+    Ok(buff)
 }
