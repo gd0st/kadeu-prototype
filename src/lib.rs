60acd51492840ec1883246d97df6bf93e25ee42d
Yannick <yannick.dorn@gmail.com>
2023-05-26 21:26:09 -0500

    Reworking the library again :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,178 +1,66 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
+#[derive(PartialEq)]
+struct SimpleCard {
+    front: String,
+    back: String,
+    tags: Vec<String>,
+}
+
+impl Card for SimpleCard {
+    fn new(front: String, back: String, tags: Vec<String>) -> SimpleCard {
+        SimpleCard { front, back, tags }
+    }
+    fn push_tag(&mut self, tag: String) {
+        self.tags.push(tag);
+    }
+
+    fn flip(&self) -> SimpleCard {
+        SimpleCard {
+            front: self.back.clone(),
+            back: self.front.clone(),
+            tags: self.tags.clone(),
+        }
+    }
+}
+
+trait Card {
+    fn new(front: String, back: String, tags: Vec<String>) -> Self;
+    fn push_tag(&mut self, tag: String);
+    fn flip(&self) -> Self;
+}
+
+impl Validator for SimpleCard {
+    fn is_valid(&self, tag: &String) -> bool {
+        self.back == *tag
+    }
+}
 
 trait Validator {
-    fn valid(&self, answer: &Answer) -> bool;
-}
-
-#[derive(Deserialize)]
-pub struct Deck {
-    pub title: String,
-    tags: Option<Vec<String>>,
-    description: Option<String>,
-    cards: Vec<Card>,
-}
-
-impl Deck {
-    pub fn new(title: String, cards: Vec<Card>) -> Deck {
-        Deck {
-            title,
-            cards,
-            tags: None,
-            description: None,
-        }
-    }
-
-    pub fn title(&self) -> &String {
-        &self.title
-    }
-    pub fn cards(&self) -> Vec<&Card> {
-        let mut cache: Vec<&Card> = Vec::new();
-        for card in &self.cards {
-            cache.push(card)
-        }
-        cache
-    }
-}
-
-#[derive(Deserialize)]
-#[serde(untagged)]
-pub enum Tag {
-    Single(String),
-    Many(Vec<String>),
-}
-#[derive(Deserialize)]
-pub struct Card {
-    tags: Option<Tag>,
-    challenge: String,
-    target: Target,
-}
-
-impl Card {
-    pub fn new(challenge: String, target: Target) -> Card {
-        Card {
-            challenge,
-            target,
-            tags: None,
-        }
-    }
-
-    pub fn challenge(&self) -> &str {
-        &self.challenge
-    }
-
-    pub fn answers(&self) -> Vec<&String> {
-        match &self.target {
-            Target::Simple(target) => vec![&target],
-            Target::Multi(targets) => targets.iter().collect(),
-        }
-    }
-
-    pub fn validate(&self, answer: &String) -> bool {
-        self.target.is_valid(answer)
-    }
-}
-
-impl Target {
-    pub fn is_valid(&self, input: &str) -> bool {
-        match self {
-            Target::Simple(target) => input == target,
-            Target::Multi(targets) => targets.iter().any(|target| *target == *input),
-        }
-    }
-}
-
-impl From<Vec<String>> for Target {
-    fn from(targets: Vec<String>) -> Target {
-        Target::Multi(targets)
-    }
-}
-impl From<String> for Target {
-    fn from(target: String) -> Target {
-        Target::Simple(target)
-    }
-}
-
-pub struct Answer(String);
-
-impl Validator for Card {
-    fn valid(&self, answer: &Answer) -> bool {
-        self.target.is_valid(answer.0.as_str())
-    }
-}
-
-#[derive(Deserialize)]
-#[serde(untagged)]
-pub enum Target {
-    Simple(String),
-    Multi(Vec<String>),
-}
-
-mod game {
-    use super::{Card, Deck};
-
-    pub struct Practice {
-        deck: Deck,
-    }
-
-    enum GameMode {
-        Practice,
-    }
-
-    ///! docs
-    ///!
-    ///! The rules of the game follow a simple 8 bit pattern that
-    ///! Will dictate what is allowed during the game.
-    ///! So for instance 1101 is practice mode.
-    ///! 0111 is a soft test
-    ///! 1010 Would be a hard test
-    struct GameRules(u8);
-    pub struct Game {
-        deck: Deck,
-        mode: GameRules,
-    }
+    fn is_valid(&self, tag: &String) -> bool;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::game::Game;
-    use super::game::Practice;
-    use super::*;
-    use serde_json;
+    use super::SimpleCard;
     #[test]
-    fn de_into_single_target_card() {
-        let input = r#"
-            {"challenge": "foobar", "target": "bizz", "tags": ["bazz"]}
-        "#;
-        let card: Card = serde_json::from_str(input).unwrap();
-        let answer = Answer("bizz".to_string());
-        assert!(card.valid(&answer))
+    fn make_card() {
+        let front: String = "Foo".into();
+        let back: String = "Bar".into();
+        let tags: Vec<String> = vec![];
+        let card = SimpleCard::new(front, back, tags);
+        assert_eq!(card.back, "Bar".to_string());
+        assert_eq!(card.front, "Foo".to_string());
+        assert!(card.tags.is_empty());
     }
 
     #[test]
-    fn de_into_multiple_target_card() {
-        let input = r#"
+    fn make_card_and_push_tag() {
+        let front: String = "Foo".into();
+        let back: String = "Bar".into();
+        let tags: Vec<String> = vec![];
+        let mut card = SimpleCard::new(front, back, tags);
+        assert!(card.tags.is_empty());
 
-{"challenge": "foo", "target": ["bar", "bazz"], "tags": ["bizz"]}
-
-"#;
-        let card: Card = serde_json::from_str(input).unwrap();
-
-        let answer = Answer("bar".to_string());
-
-        assert!(card.valid(&answer));
-    }
-
-    #[test]
-    fn build_practice_game() {
-        let cards = vec![Card::new(
-            "foobar".to_string(),
-            Target::Simple("bazz".to_string()),
-        )];
-
-        let deck = Deck::new("Simple deck for foo.".to_string(), cards);
-
-        let practice_game = game::Practice::new(deck);
-        assert!(true);
+        card.push_tag("Bar".into());
+        assert_eq!(card.tags.len(), 1);
     }
 }
