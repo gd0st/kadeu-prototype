7b10c87df20aa60ea109814e9434462a498d2fc8
Yannick Dorn <yannick.dorn@gmail.com>
2023-06-18 14:45:56 -0500

    more

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,67 @@
 use std::collections::HashMap;
 
-// Card would need to be an interface if I want to easily spit them
-// out of an iterator
-struct SimpleCard(String, String);
-impl Card for SimpleCard {
-    fn new(front: String, back: String) -> SimpleCard {
-        SimpleCard(front, back)
+pub enum Value {
+    Simple(String),
+    Complex(Vec<String>),
+}
+
+impl Value {
+    fn resolve_answer(self) -> Option<String> {
+        match self {
+            Value::Simple(s) => Some(s),
+            // Not ideal
+            Value::Complex(mut ss) => ss.pop(),
+        }
     }
 }
-trait Card {
-    fn new(front: String, back: String) -> Self;
+
+// Card would need to be an interface if I want to easily spit them
+// out of an iterator
+pub struct SimpleCard {
+    key: String,
+    value: String,
 }
-struct Deck<T: Card>(Vec<T>);
+impl SimpleCard {
+    pub fn front(&self) -> &String {
+        &self.key
+    }
+    pub fn back(&self) -> &String {
+        &self.value
+    }
+}
+impl Card for SimpleCard {
+    fn new(front: String, back: String) -> SimpleCard {
+        SimpleCard {
+            key: front,
+            value: back,
+        }
+    }
+    // generic new function, but handeld by a factory?
+    fn dynamic_new(front: String, back: Value) -> SimpleCard {
+        match back {
+            Value::Simple(string) => SimpleCard {
+                key: front,
+                value: string,
+            },
+            Value::Complex(mut backs) => {
+                let value: String = backs.pop().unwrap_or("foobar".to_string());
+                SimpleCard { key: front, value }
+            }
+        }
+    }
+}
+
+pub trait Card {
+    fn new(front: String, back: String) -> Self;
+    fn dynamic_new(front: String, back: Value) -> Self;
+}
+pub struct Deck<T: Card>(Vec<T>);
+
+impl<T: Card> Deck<T> {
+    pub fn new(cards: Vec<T>) -> Deck<T> {
+        Deck(cards)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -20,7 +70,7 @@ mod tests {
     fn make_simple_card() {
         let key = "foo".to_string();
         let value = "bar".to_string();
-        let card = SimpleCard(key, value);
+        let card = SimpleCard::new(key, value);
         assert_eq!(card.0, String::from("foo"));
         assert_eq!(card.1, String::from("bar"));
     }
@@ -29,7 +79,7 @@ mod tests {
     fn make_deck() {
         let key = "foo".to_string();
         let value = "bar".to_string();
-        let card = SimpleCard(key, value);
+        let card = SimpleCard::new(key, value);
         let _ = Deck(vec![card]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
 use clap::Parser;
-use std::io;
+use kadeu::{Card, Deck, SimpleCard};
+use std::io::{self, Write};
+
+// Need simple app that can tell stdout what to render next.
+// And receives input, need to read more code before I do that.
 
 #[derive(Debug, Parser)]
 #[command(author, version)]
 struct GameArgs {
     // TODO allow for flashcard csv file.
-    #[arg(long)]
-    flashcards: String,
-    #[arg(long)]
-    shuffle: bool,
+    //#[arg(long)]
+    //flashcards: String,
+    //#[arg(long)]
+    //shuffle: bool,
 }
 fn main() {
     let args = GameArgs::parse();
 
-    let flashcards_filepath = args.flashcards;
-    println!("{}", &flashcards_filepath);
+    let card: SimpleCard = SimpleCard::new(
+        "What group of organism is a fungus".to_string(),
+        "eukaryotic".to_string(),
+    );
+    let next_card = SimpleCard::new(
+        "What is Cytahus collocially known as?".to_string(),
+        "Bird's nest fungi".to_string(),
+    );
 
+    let cards = vec![card, next_card];
+
+    for card in cards {
+        println!("!> {}", card.front());
+        print!("?> ");
+        let mut answer = String::new();
+        io::stdout().flush().expect("stdout flush");
+        read_to_buff(&mut answer);
+        println!("> {}", card.back());
+        // No reason to ask if yay or no if no benefit
+        io::stdout().flush().expect("stdout flush");
+        // just print the next question or wait for a continue?
+        // might be useful to have when going into more advanced terminal interfaces.
+        read_to_buff(&mut answer);
+
+        // answer
+        // feedback
+        // continue
+    }
+    //let flashcards_filepath = args.flashcards;
     // TODO flashcard reader needs to be implemented after core game loop...
 }
 
@@ -23,7 +53,6 @@ fn read_to_buff(buff: &mut String) {
     let _ = io::stdin()
         .read_line(buff)
         .expect("Attempting stdio input.");
-    println!("here");
 
     if let Some('\n') = buff.chars().next_back() {
         buff.pop();
