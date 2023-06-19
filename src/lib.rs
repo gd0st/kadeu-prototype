bf57006dcf5cbf786122d44fcac30033ce3b49b9
gd0st <yannick.dorn@gmail.com>
2023-06-19 01:38:03 -0500

    playing around with new trait implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,39 @@
 use std::collections::HashMap;
+use std::fmt::Display;
 
-pub enum Value {
-    Simple(String),
-    Complex(Vec<String>),
-}
 
-impl Value {
-    fn resolve_answer(self) -> Option<String> {
-        match self {
-            Value::Simple(s) => Some(s),
-            // Not ideal
-            Value::Complex(mut ss) => ss.pop(),
-        }
-    }
+
+pub trait CardMaker {
+    type BACK;
+    fn new(front: String, back: Self::BACK) -> Self;
 }
 
-// Card would need to be an interface if I want to easily spit them
-// out of an iterator
-pub struct SimpleCard {
-    key: String,
-    value: String,
+pub fn validate<T: PartialEq>(input: T, answer: T) -> bool {
+    input == answer
 }
-impl SimpleCard {
-    pub fn front(&self) -> &String {
-        &self.key
-    }
-    pub fn back(&self) -> &String {
-        &self.value
-    }
+
+struct Card<T> {
+    front: String,
+    back: T
 }
-impl Card for SimpleCard {
-    fn new(front: String, back: String) -> SimpleCard {
-        SimpleCard {
-            key: front,
-            value: back,
-        }
-    }
-    // generic new function, but handeld by a factory?
-    fn dynamic_new(front: String, back: Value) -> SimpleCard {
-        match back {
-            Value::Simple(string) => SimpleCard {
-                key: front,
-                value: string,
-            },
-            Value::Complex(mut backs) => {
-                let value: String = backs.pop().unwrap_or("foobar".to_string());
-                SimpleCard { key: front, value }
-            }
-        }
+
+
+impl CardMaker for Card<String> {
+    type BACK = String;
+    fn new(front: String, back: String) -> Card<String> {
+        Card { front, back }
     }
 }
 
-pub trait Card {
-    fn new(front: String, back: String) -> Self;
-    fn dynamic_new(front: String, back: Value) -> Self;
-}
-pub struct Deck<T: Card>(Vec<T>);
 
-impl<T: Card> Deck<T> {
-    pub fn new(cards: Vec<T>) -> Deck<T> {
-        Deck(cards)
-    }
-}
+//pub struct Deck<T: Card>(Vec<T>);
+//impl<T: Card> Deck<T> {
+    //pub fn new(cards: Vec<T>) -> Deck<T> {
+        //Deck(cards)
+    //}
+//}
 
 #[cfg(test)]
 mod tests {
-    use super::{Card, Deck, SimpleCard};
-    #[test]
-    fn make_simple_card() {
-        let key = "foo".to_string();
-        let value = "bar".to_string();
-        let card = SimpleCard::new(key, value);
-        assert_eq!(card.0, String::from("foo"));
-        assert_eq!(card.1, String::from("bar"));
-    }
 
-    #[test]
-    fn make_deck() {
-        let key = "foo".to_string();
-        let value = "bar".to_string();
-        let card = SimpleCard::new(key, value);
-        let _ = Deck(vec![card]);
-    }
 }
